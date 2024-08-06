e412b212909a34e1ff56d1eb1344abf49c3a4a9d
Regha <iamregha@gmail.com>
2024-08-06 23:43:39 +0100

    add files

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from blogforms import UsersForm, LoginForm, PostsForm, SearchForm
 app = Flask(__name__)
 app.config.from_object('config.ProConfig')
 #app.config.from_object('config.DevConfig')
-# app.config.from_pyfile('config.py')
+#app.config.from_pyfile('config.py')
 # initialize ckeditor
 ckeditor = CKEditor(app)
 
